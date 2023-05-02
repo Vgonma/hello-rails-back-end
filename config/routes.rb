6c32e83c7bd2e295bd4bbdfbe4a9b7d2c53285b0
Victor <victorgmassimi.microverse@gmail.com>
2023-05-02 15:44:16 -0600

    setup greetings api endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'api/greetings#index'
+  
+  namespace :api do
+    resources :greetings, only: [:index]
+    get '/greetings/random', to: 'greetings#random'
+  end
 end
